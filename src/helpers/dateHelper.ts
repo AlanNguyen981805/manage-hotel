49ef7f5771a1aca0483125dff8cf361e4478c505
Hiep Nguyen Ngoc (SDC3.BU2) <hiep.nguyen5@ntq-solution.com.vn>
2025-03-01 15:49:24 +0700

    refactor: change type of data

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -1,7 +1,9 @@
-export const calculateDaysBetweenDates = (date1: Date, date2: Date): number => {
+export const calculateDaysBetweenDates = (
+  date1: Date | null,
+  date2: Date | null
+): number => {
   if (!date1 || !date2) return 0;
-
   const dayInMs = 24 * 60 * 60 * 1000; // Số mili giây trong một ngày
-  const diffInMs = Math.abs(new Date(date2) - new Date(date1)); // Chênh lệch thời gian giữa hai ngày
+  const diffInMs = Math.abs(date2.getTime() - date1.getTime()); // Chênh lệch thời gian giữa hai ngày
   return Math.floor(diffInMs / dayInMs) + 1; // Chia chênh lệch cho số mili giây trong một ngày và cộng thêm 1 để tính đủ cả hai ngày
 };
