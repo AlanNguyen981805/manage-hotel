b492b27f03d2f26f36784c0d36180268a87cbbdd
Hiep Nguyen Ngoc (SDC3.BU2) <hiep.nguyen5@ntq-solution.com.vn>
2025-03-01 15:27:46 +0700

    refactor: change type of data

--- src/components/SearchResult/components/Services/index.tsx
+++ src/components/SearchResult/components/Services/index.tsx
@@ -1,31 +1,7 @@
-import Dropdown from "@/components/Dropdown";
-
 export const Services = () => {
-  const cities = [
-    {
-      name: "Ha Noi",
-      value: 1,
-    },
-    {
-      name: "TP H.C.M",
-      value: 2,
-    },
-    {
-      name: "Hai Phong",
-      value: 3,
-    },
-  ];
-
   return (
     <div className="flex my-3">
       <p>Typeof car</p>
-      <Dropdown options={cities} name="city" />
-
-      <p>1 chieu</p>
-      <Dropdown options={cities} name="city" />
-
-      <p>2 chieu</p>
-      <Dropdown options={cities} name="city" />
 
       <p className="text-2xl"> +</p>
     </div>
